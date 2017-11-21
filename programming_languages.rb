c64daa99fb6584198991fca97be8c4c9a4188fed
Kenny Hom <kenny.hom27@gmail.com>
2017-11-21 03:49:53 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,10 +2,10 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |type, lang|
     lang.map do |k, v|
-      new_hash[k]= v
-      new_hash[k][:style] = [type]
+      new_hash[k] ||= v
+      new_hash[k][:style] ||= []
+      new_hash[k][:style] << type
     end
   end
-  new_hash[:javascript][:style].unshift(:oo)
   new_hash
 end
