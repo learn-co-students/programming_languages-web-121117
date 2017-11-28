7adeba1afe0420c040b0f8620b26f6dc149d9295
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-11-28 02:30:41 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,20 @@
-def reformat_languages(languages)
-  # your code here
+require 'pry'
+
+def reformat_languages(languages_by_style)
+  hash = Hash.new
+  languages_by_style.each do |styles|
+    styles.each_with_index do |style, i|
+      styles[1].each do |language, types|
+        hash[language] = {}
+          types.each do |type, value|
+          hash[language] = {type => value}
+          hash[language][:style] = [styles[0]]
+          if language == :javascript
+            hash[language][:style] = languages_by_style.keys
+          end
+        end
+      end
+    end
+  end
+  return hash
 end
