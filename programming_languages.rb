e01fdcd7d1db67f2631c18f7be43971ef04eaab2
josephdlawson21 <josephdlawson21@gmail.com>
2017-11-30 11:51:43 -0500

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,18 @@
-def reformat_languages(languages)
-  # your code here
+require "pry"
+def reformat_languages(arr)
+  result = {}
+  hold =[]
+  arr.each do |key,value|
+    value.each do |lang, type|
+      result[lang] = type
+    end
+  end
+  result.each do |lang,info|
+    result[lang][:style] = []
+    arr.each do |key, value|
+      if value.has_key?(lang)
+        result[lang][:style] << key
+      end
+    end
+  end
 end
