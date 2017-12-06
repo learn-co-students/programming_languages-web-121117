cab428465947fe6d2869fcd9a849e549715a4fc3
github username <github email address>
2017-12-06 17:09:32 -0500

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
+require "pry"
+
+#hash_key puts oo and functional
+#hash_data puts the whole hash of the lanuages and their type
 def reformat_languages(languages)
-  # your code here
+new_hash = Hash.new
+# another_hash = Hash.new
+languages.each do |hash_key, hash_data|
+  hash_data.each do |language_name, types|
+     new_hash[language_name] = types
+     new_hash[language_name][:style] = [hash_key]
+    end
+    new_hash[:javascript][:style] << :oo
+  end
+  new_hash
 end
